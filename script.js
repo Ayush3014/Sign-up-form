4770f783059218400d272041cf7da90ce9d1fb6f
Ayush <ayushpanchal005@gmail.com>
2023-07-16 19:04:37 +0530

    Added checks for password and its confirmation

--- script.js
+++ script.js
@@ -1,17 +1,48 @@
-/*
-How you attack this project is mostly up to you, but it is wise to begin by scaffolding out the structure of the page, and then tackle the various sections one by one.
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const button = document.getElementById('submitButton');
 
-The area behind the “ODIN” logo is a div that has a dark, but semi-transparent background color. This enhances the readability of the text against the busy background image.
+email.addEventListener('input', () => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('I am expecting an email address!');
+  } else {
+    email.setCustomValidity('');
+  }
+});
 
-The color we’ve chosen for the ‘Create Account’ button is similar to tones found in the background image. Specifically, it is #596D48.
+password.addEventListener('input', () => {
+  if (password.value.length >= 8) {
+    password.setCustomValidity('');
+  } else {
+    password.setCustomValidity('Password should have at least 8 characters.');
+  }
+});
 
-The inputs, by default have a very light border (#E5E7EB), but we’ve included 2 variations. For starters, the password inputs should be given an error class.
+confirmPassword.addEventListener('input', () => {
+  if (confirmPassword.value.length >= 8) {
+    confirmPassword.setCustomValidity('');
+  } else {
+    confirmPassword.setCustomValidity(
+      'Password should have at least 8 characters.'
+    );
+  }
+});
 
-The other variation is the selected input, which should have a blue border and subtle box-shadow. This can be done with the :focus pseudo-class you’ve learned about in an earlier lesson.
+button.addEventListener('click', (event) => {
+  if (
+    password.validity.valid &&
+    confirmPassword.validity.valid &&
+    password.value === confirmPassword.value
+  ) {
+    button.style.backgroundColor = 'green';
+    button.textContent = 'Welcome!';
+  } else {
+    button.textContent = 'The passwords do not match!';
+    button.style.backgroundColor = 'red';
+    password.value = '';
+    confirmPassword.value = '';
+  }
 
-Do not worry about making your project look nice on mobile, but DO resize your browser a little bit to make sure that it’s not completely broken at different desktop resolutions.
-
-Checking that the password fields match each other requires javascript. Try to implement this if you feel confident, otherwise just validate each field separately.
-*/
-
-// remaining: PASSWORD CONFIRM, INVALID ONLY AFTER USER ENTERS DATA NOT BEFORE
+  event.preventDefault();
+});
